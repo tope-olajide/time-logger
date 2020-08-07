e2bd305ca7ea4b1c06340692f8ae154545a6fe55
Temitope <tope_olajide@outlook.com>
2020-08-07 13:49:02 +0100

    Install React

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Timer from './Components/Timer'
+function App() {
+  return (
+    <>
+    <Timer />
+    </>
+  );
+}
+
+export default App;
